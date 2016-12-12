1464a978561f825f737d70dbef0812f951c9c8ae
leeneil <lishantreasure@gmail.com>
2016-12-11 22:29:24 -0800

    add predict.py

---
+++ predict.py
@@ -0,0 +1,351 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
+from IPython import embed
+import psana
+import h5py
+import sys, os
+import time
+#os.environ['THEANO_FLAGS']='mode=FAST_RUN,device=gpu,floatX=float32'
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+
+import matplotlib.pyplot as plt
+from pyimgalgos.MedianFilter import median_filter_ndarr
+import pylab as pl
+import matplotlib.cm as cm
+import numpy as np
+from numpy import random
+np.random.seed(1337) # for reproducibility
+
+useFile = 1
+
+cropT = 700;
+cropB = 1050
+cropL = 700;
+cropR = 1050;
+
+def prepoces(eventNumber):
+    evt = run.event(times[eventNumber])
+    calib = det.calib(evt) * det.mask(evt, calib=True, status=True,
+                                      edges=True, central=True,
+                                      unbond=True, unbondnbrs=True)
+    # background suppression
+    medianFilterRank = 5
+    calib -= median_filter_ndarr(calib, medianFilterRank)
+    # crop
+    img = det.image(evt, calib)[cropT:cropB, cropL:cropR]  # crop inside water ring
+    # normalization
+    sig = np.std(img)
+    mu  = np.mean(img)
+    img = img - mu
+    if sig > 0.000000001:
+        img = img / sig
+    return img
+
+
+def confu_matrix(y_label, Y_predict):
+    y_predict = np.argmax(Y_predict, axis=1)
+    n_tp = np.sum( y_label * y_predict )
+    n_fp = np.sum( (1-y_label) * y_predict )
+    n_fn = np.sum( y_label * (1-y_predict) )
+    n_tn = np.sum( (1-y_label) * (1-y_predict) )
+    
+    n = y_label.shape[0]
+    
+    p_tp = 1.0 * n_tp / n
+    p_fp = 1.0 * n_fp / n
+    p_fn = 1.0 * n_fn / n
+    p_tn = 1.0 * n_tn / n
+
+    print("\t    label ")
+    print("\\\tT\t\tF\t")
+    print("T\t" + str(p_tp) + "\t" + str(p_fp))
+    print("F\t" + str(p_fn) + "\t" + str(p_tn))
+    
+    print(y_label)
+    print(y_predict)
+
+    return (p_tp, p_fp, p_fn, p_tn)
+
+
+
+
+
+
+
+import numpy.ma as ma
+def make_mosaic(imgs, nrows, ncols, border=1):
+    """
+    Given a set of images with all the same shape, makes a
+    mosaic with nrows and ncols
+    """
+    nimgs = imgs.shape[0]
+    imshape = imgs.shape[1:]
+    
+    mosaic = ma.masked_all((nrows * imshape[0] + (nrows - 1) * border,
+                            ncols * imshape[1] + (ncols - 1) * border),
+                            dtype=np.float32)
+    
+    paddedh = imshape[0] + border
+    paddedw = imshape[1] + border
+    for i in xrange(nimgs):
+        row = int(np.floor(i / ncols))
+        col = i % ncols
+        
+        mosaic[row * paddedh:row * paddedh + imshape[0],
+               col * paddedw:col * paddedw + imshape[1]] = imgs[i]
+    return mosaic
+
+
+#trainStack = np.zeros((trainSize,imgShape[0],imgShape[1]))
+#testStack = np.zeros((testSize,imgShape[0],imgShape[1]))
+# interleave misses and hits
+
+# Generate trainLabel, testLabel
+
+# Visually check labels are consistent with the images
+
+
+thr = 0
+
+# numTrain = 2400 # TODO: Set to trainSize
+# numTest = 10 # TODO: Set to testSize
+
+numIters = 1
+nb_classes = 2
+batch_size = 100
+nb_epoch = 5
+learn_rate = 0.001  # 0.002
+
+imgShape = np.array([cropB-cropT, cropR-cropL])
+
+# X_train = np.empty((numTrain, 1, imgShape[0], imgShape[1]))
+# X_test = np.empty((numTest, 1, imgShape[0], imgShape[1]))
+# y_train = np.empty((numTrain,),dtype=int)
+# y_test = np.empty((numTest,),dtype=int)
+
+#########################################
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.optimizers import SGD
+import theano
+print(theano.config.device)
+
+# build KERAS model
+print("building CNN model")
+
+
+x_train = np.empty((1, 1, imgShape[0], imgShape[1]))
+
+model = Sequential()
+
+# model.add(BatchNormalization(mode=0, input_shape=x_train.shape[1:]))
+model.add(Convolution2D(4, 7, 7, border_mode='valid', input_shape=x_train.shape[1:]))
+# The Dropout is not in the original keras example, it's just here to demonstrate how to
+# correctly handle train/predict phase difference when visualizing convolutions below
+model.add(Dropout(0.1))
+
+model.add(BatchNormalization(mode=0))
+
+convout1 = Activation('relu')
+model.add(convout1)
+
+model.add(MaxPooling2D(pool_size=(5, 5)))
+model.add(Convolution2D(4, 7, 7))
+model.add(BatchNormalization(mode=1))
+convout2 = Activation('relu')
+model.add(convout2)
+model.add(MaxPooling2D(pool_size=(5, 5)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(16))
+model.add(BatchNormalization(mode=1))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(nb_classes))
+model.add(Activation('relu'))
+sgd = SGD(lr=learn_rate, momentum=0.9, decay=0.0001)
+model.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+model.summary()
+#################################################
+
+if useFile:
+    import h5py
+    exp = 'cxic0415'
+    runNum = int( sys.argv[1] )
+    inPath = '/reg/d/psdm/cxi/cxic0415/scratch/yoon82/psocake/'
+    outPath = '/reg/d/psdm/cxi/cxitut13/scratch/liponan/'
+    detname = 'DscCsPad'
+    fname = outPath + exp + '_' + str(runNum).zfill(4) + '.h5'
+    print("loading " +  fname)
+    f = h5py.File(fname, 'r')
+else:
+    # Load H5 file
+    print("loading H5 file")
+
+    tic = time.time()
+    # f = h5py.File('/reg/d/psdm/cxi/cxitut13/res/yoon82/r0010/cxitut13_0010.cxi','r')
+    # f = h5py.File('/reg/d/psdm/cxi/cxitut13/res/yoon82/cxis0813/cxis0813_0032.cxi','r')
+    f = h5py.File('/reg/d/psdm/cxi/cxic0415/scratch/yoon82/psocake/r0099/cxic0415_0099.cxi', 'r')
+    # f = h5py.File('/dev/shm/lipon/cxis0813_0032.cxi','r')
+
+    #imgs = f['/entry_1/data_1/data']
+    numIndex = f['/entry_1/result_1/nPeaksAll'].value
+    #numIndex = numIndex[ numIndex!=-2 ]
+    f.close()
+    toc = time.time()
+    print("Time to read in hdf5: ",toc-tic)
+
+    print(numIndex)
+
+    ds = psana.DataSource('exp=cxic0415:run=99:idx')
+    run = ds.runs().next()
+    times = run.times()
+    numEvents = len(times)
+    env = ds.env()
+    eventNumber = 0
+    evt = run.event(times[eventNumber])
+    det = psana.Detector('DscCsPad')
+    tic = time.time()
+    img = prepoces(eventNumber)
+    imgShape = img.shape
+    toc = time.time()
+    print("Time for preprocessing per image: ",toc-tic)
+    #plt.imshow(img)
+    #plt.show()
+
+    # Generate train/test list from quartile
+    lowerB = np.percentile(numIndex,q=25)
+    higherB = np.percentile(numIndex,q=75)
+    print("lower,higher bounds: ", lowerB, higherB)
+
+    missInd = np.where(numIndex<=lowerB)[0]
+    hitInd = np.where(numIndex>=higherB)[0]
+    print("number of misses, hits: ", len(missInd), len(hitInd))
+
+    # Split into training / testing sets
+    testSize = 50
+    # TODO: add assert 50
+    missInd_test = missInd[-testSize:]
+    hitInd_test = hitInd[-testSize:]
+    missInd_train = missInd[:len(missInd)-testSize]
+    hitInd_train = hitInd[:len(hitInd)-testSize]
+
+    trainSize = len(missInd_train) + len(hitInd_train)
+    testSize = 2 * testSize
+    print("Available train and test images: ", trainSize, testSize)
+
+
+numTrain = 2*np.minimum( f["/data/missTrain"].shape[0], f["/data/hitTrain"].shape[0] ) # TODO: Set to trainSize
+numTest = f["/data/missTest"].shape[0] + f["/data/hitTest"].shape[0] # TODO: Set to testSize
+numPredict = numTrain + numTest
+
+
+print("In the original data:")
+print("# of miss in training data: ", f["/data/missTrain"].shape[0])
+print("# of hit  in training data: ", f["/data/hitTrain"].shape[0])
+print("# of miss in testing  data: ", f["/data/missTest"].shape[0])
+print("# of hit  in testing  data: ", f["/data/hitTest"].shape[0])
+
+print("# of final prediction data: ", numPredict)
+
+X_val = np.empty((numPredict, 1, imgShape[0], imgShape[1]))
+y_val = np.empty((numPredict,),dtype=int)
+
+
+
+# prepare testing set
+counter_missTest = 0
+counter_hitTest = 0
+for u in range(0, numTest):
+    if u % 2 == 0:
+        if useFile:
+            img = f["/data/missTest"][counter_missTest, :, :, :]
+        else:
+            img = prepoces(missInd_test[counter_missTest])
+        y_val[u] = 0
+        counter_missTest += 1
+    else:
+        if useFile:
+            img = f["/data/hitTest"][counter_hitTest, :, :, :]
+        else:
+            img = prepoces(hitInd_test[counter_hitTest])
+        y_val[u] = 1
+        counter_hitTest += 1
+    ### sample-wise normalization ###
+    #std = np.std(img)
+    #mu = np.mean(img)
+    #img = (img-mu) / std
+    #################################
+    # print(img.shape)
+    # img_reshape = np.reshape(img, [1, img.shape[0], img.shape[1]])
+    X_val[u, :, :, :] = img
+
+
+counter_miss = 0
+counter_hit = 0
+if useFile:
+    mInd = np.random.permutation(f["/data/missTrain"].shape[0])
+    hInd = np.random.permutation(f["/data/hitTrain"].shape[0])
+else:
+    mInd = np.random.permutation(len(missInd_train))
+    hInd = np.random.permutation(len(hitInd_train))
+# prepare training set
+for u in range(0, numTrain):
+    if u % 2 == 0:
+        if useFile:
+            img = f["/data/missTrain"][mInd[counter_miss],:,:,:]
+        else:
+            img = prepoces(missInd_train[mInd[counter_miss]])
+        y_val[(u+numTest)] = 0
+        counter_miss += 1
+    else:
+        if useFile:
+            img = f["/data/hitTrain"][hInd[counter_hit],:,:,:]
+        else:
+            img = prepoces(hitInd_train[hInd[counter_hit]])
+        y_val[(u+numTest)] = 1
+        counter_hit += 1
+    ### sample-wise normalization ###
+    # std = np.std(img)
+    # mu = np.mean(img)
+    # img = (img-mu) / std
+    #################################
+    # img_reshape = np.reshape(img, [1, img.shape[0], img.shape[1]])
+    X_val[(u+numTest),:,:,:] = img
+
+#print(y_train)
+print("% of hit in validation set: ", np.sum(y_val)/(len(y_val)*1.0))
+
+Y_val = np_utils.to_categorical(y_train, nb_classes)
+
+
+
+if len(sys.argv) > 2:
+    filename = sys.argv[2]
+else:
+    filename = "weights.hdf"
+
+
+print("loading " + filename)
+model.load_weights(filename)
+
+
+Y_predict = model.predict(X_val, batch_size=100, verbose=1)
+print("Prediction result")
+confu_matrix(y_val, Y_val)
+
+
+if useFile: f.close()
+
+
+
+# Visualize weights
+
